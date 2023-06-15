f537dad98a2dc8de1c8edea5858519d6848bd5c3
Ilyes <ilyes.aidoudi@hotmail.com>
2023-06-15 15:57:34 +0200

    16:00

---
+++ BOXE/Combat.h
@@ -0,0 +1,64 @@
+#pragma once
+class Combat {
+private:
+    std::string niveau;
+    std::string adr;
+    Boxeur* coinBleu;
+    Boxeur* coinRouge;
+    Boxeur* vainqueur;
+
+public:
+    Combat(const std::string& niveau, const std::string& adr) : niveau(niveau), adr(adr), coinBleu(nullptr), coinRouge(nullptr), vainqueur(nullptr) {}
+
+    std::string getNiveau() const {
+        return niveau;
+    }
+
+    std::string getAdr() const {
+        return adr;
+    }
+
+    Boxeur* getCoinBleu() const {
+        return coinBleu;
+    }
+
+    Boxeur* getCoinRouge() const {
+        return coinRouge;
+    }
+
+    void setCoinBleu(Boxeur* boxeur) {
+        if (boxeur != coinRouge) {
+            coinBleu = boxeur;
+        }
+        else {
+            std::cout << "Erreur : Le même boxeur ne peut pas être dans les deux coins !" << std::endl;
+        }
+    }
+
+    void setCoinRouge(Boxeur* boxeur) {
+        if (boxeur != coinBleu) {
+            coinRouge = boxeur;
+        }
+        else {
+            std::cout << "Erreur : Le même boxeur ne peut pas être dans les deux coins !" << std::endl;
+        }
+    }
+
+    Boxeur* getVainqueur() const {
+        return vainqueur;
+    }
+
+    Boxeur* DesignerVainqueur(std::string couleurCoin) {
+        if (couleurCoin == "bleu") {
+            vainqueur = coinBleu;
+        }
+        else if (couleurCoin == "rouge") {
+            vainqueur = coinRouge;
+        }
+        else {
+            vainqueur = nullptr;
+        }
+
+        return vainqueur;
+    }
+};
---
+++ BOXE/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+
+
+
+int main() {
+    // Instanciation d'un objet boxeur_1 de classe Boxeur (automatique)
+    Boxeur boxeur_1("Marc", "26 Avenue Tony Garnier 69007", 75.0, 0.0);
+
+    // Affichage des informations de boxeur_1
+    std::cout << "Nom du boxeur 1 : " << boxeur_1.getNom() << std::endl;
+    std::cout << "Poids du boxeur 1 : " << boxeur_1.getPoids() << " kg" << std::endl;
+    std::cout << "Adresse du boxeur 1 : " << boxeur_1.getAdr() << std::endl;
+
+    // Instanciation d'un objet boxeur_2 de classe Boxeur (dynamique)
+    Boxeur* boxeur_2 = new Boxeur("Sam", "69 Boulevard Lenine 69220", 78.0, 0.0);
+
+    // Affichage des informations de boxeur_2
+    std::cout << "\nNom du boxeur 2 : " << boxeur_2->getNom() << std::endl;
+    std::cout << "Poids du boxeur 2 : " << boxeur_2->getPoids() << " kg" << std::endl;
+    std::cout << "Adresse du boxeur 2 : " << boxeur_2->getAdr() << std::endl;
+
+    // Instanciation d'un objet combat_1 de classe Combat (automatique)
+    Combat combat_1("1/2 de finale", "Adresse du combat");
+
+    // Association entre combat_1 et boxeur_1
+    combat_1.setCoinBleu(&boxeur_1);
+
+    // Association entre combat_1 et boxeur_2
+    combat_1.setCoinRouge(boxeur_2);
+
+    // Affichage des informations de combat_1
+    std::cout << "\nLes combats de boxe vont bientot commencer, nous allons assister a " << combat_1.getCoinBleu()->getNom() << " vs " << combat_1.getCoinRouge()->getNom() << " dans la salle : " << combat_1.getAdr() << std::endl;
+    std::cout << "\nLe boxeur qui prend le coin bleu est : " << combat_1.getCoinBleu()->getNom() << std::endl;
+    std::cout << "Le boxeur qui prend le coin rouge est : " << combat_1.getCoinRouge()->getNom() << std::endl;
+
+    // Définition du vainqueur du combat
+    combat_1.DesignerVainqueur("bleu");
+
+    // Affichage du vainqueur du combat
+    if (combat_1.getVainqueur() != nullptr) {
+        std::cout << "Le vainqueur du combat est : " << combat_1.getVainqueur()->getNom() << std::endl;
+    }
+    else {
+        std::cout << "Aucun vainqueur pour le moment." << std::endl;
+    }
+
+    // Libération de la mémoire allouée dynamiquement
+    delete boxeur_2;
+
+    return 0;
+}
